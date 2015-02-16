56e688166957906f8487ab75709c2abc88c1f7eb
Graham Christensen <graham@grahamc.com>
2015-02-16 12:10:56 -0600

    Shim in to the constructor directly, and quite rudely, I might add.

--- boto_s3_shim/__init__.py
+++ boto_s3_shim/__init__.py
@@ -1,5 +1,4 @@
 import os
-import boto
 from urllib.parse import urlparse
 from boto.s3.connection import S3Connection
 from boto.s3.connection import OrdinaryCallingFormat
@@ -9,12 +8,13 @@ override = os.environ.get('S3_OVERRIDE', False)
 if override:
     override_to = urlparse(str(override))
 
-    def conn_s3(aws_access_key_id=None, aws_secret_access_key=None, **kwargs):
+    old_init = S3Connection.__init__
+    def new__init__(self, *k, **kwargs):
         kwargs['host'] = override_to.hostname
         kwargs['port'] = int(override_to.port)
         kwargs['calling_format'] = OrdinaryCallingFormat()
         kwargs['is_secure'] = override_to.scheme == 'secure'
+        old_init(self, *k, **kwargs)
 
-        return S3Connection(aws_access_key_id, aws_secret_access_key, **kwargs)
+    S3Connection.__init__ = new__init__
 
-    boto.connect_s3 = conn_s3
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="boto-s3-shim",
-    version="0.0.3",
+    version="0.0.4",
     author="Graham Christensen",
     author_email="graham@grahamc.com",
     description="Inject an alternative S3 server into any boto-based project",
